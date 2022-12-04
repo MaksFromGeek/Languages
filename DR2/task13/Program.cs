3e71ab74be0af3b364d8f2fe0df8ab90dda5f4e1
«Maks» <the7usinessman23@gmail.com>
2022-12-04 18:31:46 +0500

    Save

--- DR2/task13/Program.cs
+++ DR2/task13/Program.cs
@@ -17,3 +17,22 @@ else
     Console.WriteLine($"Третьей цифры нет");
 }
 
+
+
+/* Console.WriteLine("Введите трех-значное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int k = 0, d = 0;
+if (number > 99)
+{
+    k = number;
+    while (k >= 1000)
+    {
+        k /= 10;
+        d = k % 10;
+    }
+Console.WriteLine($"The third number from the left is {d}");
+}
+else
+{
+   Console.WriteLine($"The number has fewer than 3 digits, sorry."); 
+} */
