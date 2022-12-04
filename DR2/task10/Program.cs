af5bdb5e0d1955ba596752e461f0f0076d9d4884
«Maks» <the7usinessman23@gmail.com>
2022-12-04 17:08:59 +0500

    Save

--- DR2/task10/Program.cs
+++ DR2/task10/Program.cs
@@ -3,7 +3,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 int d = 0;
 if (num > 99)
 {
-    while (num >= 1000)
+    for (int i = num; i >= 1000; i--)
     {
         num /= 10;
     }
@@ -18,3 +18,21 @@ else
 {
     Console.WriteLine($"Третьей цифры нет");
 }
+
+/* Console.WriteLine ("Введите любое трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int d = 0;
+if (num > 99)
+{
+    while (num >= 1000)
+    {
+        num /= 10;
+    }
+    if (num >= 100 && num <= 999)
+    {
+        num /= 10;
+        d = num % 10;
+    } */
+
+
+
