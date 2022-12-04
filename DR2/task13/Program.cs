05e758a21c86e2221ea47b5de1529e2dcab9a1dc
«Maks» <the7usinessman23@gmail.com>
2022-12-04 18:18:50 +0500

    Save

--- DR2/task13/Program.cs
+++ DR2/task13/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите любое целое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+    for (int i = num; num >= 1000; i=i/10)
+    {
+        num /= 10;
+    }
+    if (num >= 100 && num <= 999)
+    {
+        num = num % 10;
+    }
+    Console.WriteLine($"{num}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
+
