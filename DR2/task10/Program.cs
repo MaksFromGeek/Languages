06fcfb81f11a061ddad7adcb4b8cccce86986db2
«Maks» <the7usinessman23@gmail.com>
2022-12-04 15:51:19 +0500

    Save

--- DR2/task10/Program.cs
+++ DR2/task10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("Введите любое трехзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (num > 99)
+{
+    k = num;
+    while (k >= 1000)
+    {
+        k/=10;
+      //  d = k %10;
+    }
+    if (k >= 100 && k <= 999)
+    {
+        k/=10;
+        d = k%10;
+    }
+    Console.WriteLine($"{d}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
