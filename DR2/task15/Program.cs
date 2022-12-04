d85e0cf98c0b199f38305f0f6a646163cfac1552
«Maks» <the7usinessman23@gmail.com>
2022-12-04 19:05:46 +0500

    Save

---
+++ DR2/task15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите порядковый номер дня недели:");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("Не выходной");
+        break;
+    case 2:
+        Console.WriteLine("Не выходной");
+        break; 
+    case 3:
+        Console.WriteLine("Не выходной");
+        break; 
+    case 4:
+        Console.WriteLine("Не выходной");
+        break; 
+    case 5:
+        Console.WriteLine("Не выходной");
+        break; 
+    case 6:
+        Console.WriteLine("Выходной");
+        break; 
+    case 7:
+        Console.WriteLine("Выходной");
+        break; 
+    default:
+        Console.WriteLine("Не является днем недели");
+        break; 
+}
