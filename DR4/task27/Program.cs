4cbfa6da9d026c6da55329bd356196b79905f8b3
«Maks» <the7usinessman23@gmail.com>
2022-12-10 00:52:58 +0500

    Save

---
+++ DR4/task27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11; 82 -> 10; 9012 -> 12
+System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int res = 0;
+while (n>0)
+{
+    res = n%10;
+    sum = sum + res;
+    n/=10;
+}
+System.Console.WriteLine($"Сумма цифр = {sum}");
+
+
