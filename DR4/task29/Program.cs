85bee53020281c21669c53e10c19cef0fcbc62c0
«Maks» <the7usinessman23@gmail.com>
2022-12-10 01:07:55 +0500

    Save

---
+++ DR4/task29/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+Random rnd = new Random();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0,100);
+    System.Console.WriteLine($"{array[i]} ");
+}
