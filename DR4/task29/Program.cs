6bf8a0fb2b8ebde91a7e8b0d831ac9c2d51bef6a
«Maks» <the7usinessman23@gmail.com>
2022-12-10 02:31:15 +0500

    Save

--- DR4/task29/Program.cs
+++ DR4/task29/Program.cs
@@ -6,3 +6,17 @@ for (int i = 0; i < array.Length; i++)
     array[i] = rnd.Next(0,100);
     System.Console.WriteLine($"{array[i]} ");
 }
+
+
+// Console.WriteLine("Введите восемь целых чисел, после ввода каждого числа нажмите 'enter'."); 
+// int[] array = new int [8]; 
+// for (int i=0; i<8; i++) 
+// array[i]=Convert.ToInt32(Console.ReadLine()); 
+// PrintArray(array); 
+ 
+// void PrintArray(int[] a) 
+// { 
+//     for (int i = 0; i < a.Length - 1; i++) 
+//         Console.Write($"{a[i]}, "); 
+//     Console.WriteLine($"{a[^1]}"); 
+// }
