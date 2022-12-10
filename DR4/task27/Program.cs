9c41a6f5194306e1316faba0ef1d0aa4c2a6e63e
«Maks» <the7usinessman23@gmail.com>
2022-12-10 18:15:14 +0500

    Save

--- DR4/task27/Program.cs
+++ DR4/task27/Program.cs
@@ -1,15 +1,17 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
-// 452 -> 11; 82 -> 10; 9012 -> 12
-System.Console.WriteLine("Введите число");
+﻿System.Console.WriteLine("Введите целое число");
 int n = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-int res = 0;
-while (n>0)
-{
-    res = n%10;
-    sum = sum + res;
-    n/=10;
-}
-System.Console.WriteLine($"Сумма цифр = {sum}");
+System.Console.WriteLine($"Сумма цифр в веденном числе равна: {SumDigits(n)}");
 
+int SumDigits(int n)
+    {
+        int sum = 0;
+        int result = 0;
+        for (int i = 1; n > 0; i++) 
+        {
+            result = n%10;
+            sum = sum + result;
+            n/=10;
+        }
+        return sum;
+    }
 
