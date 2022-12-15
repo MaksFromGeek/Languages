cf024f527128fbd7594b3fa76ab7fb39b2773107
«Maks» <the7usinessman23@gmail.com>
2022-12-15 23:40:58 +0500

    Save

--- DR5/task36/Program.cs
+++ DR5/task36/Program.cs
@@ -2,20 +2,19 @@
 // элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0;
 
-void PrintArray(int[] array)
-{
-    foreach (int el in array) // этот цикл только для просмотра и тут нет индексов
-        System.Console.Write($"{el} ");
-    // for (int i = 0; i<array.Length; i++)
-    // Console.Write($"{arrai[i]}");
-    System.Console.WriteLine();
+void OddPlace() {
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] arr = new int[size];
+    int sum = 0;
+    for(int i=0; i<arr.Length; i++) {
+        arr[i] = new Random().Next(-10, 10);
+        if(i % 2 != 0)
+        sum += arr[i]; // складываем нечетные элементы массива
+    }
+    string SumNegative = string.Join(", ", arr);
+    System.Console.WriteLine($"Сумма элементов, стоящая на нечетных позициях в массиве [{SumNegative}] равна {sum}");
 }
-int CountNumbers(int[] array)
-{
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
-        if (array[i] % 2 == 0)
-            count++;
-    return count;
-}
+
+System.Console.WriteLine("Задайте размер массива ");
+OddPlace();
 
